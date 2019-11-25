6f704f0a203091be6c4eca6faad9d2ed8fe454b2
Lindemberg <lindembergncastro@outlook.com>
2019-11-25 15:40:54 -0300

    server

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require("koa");
+
+const Router = require("koa-router");
+
+const app = new Koa();
+
+const router = new Router();
+
+const port = 3333;
+
+const index = ctx => {
+  ctx.body = "zero ao deply";
+};
+
+const hello = ctx => {
+  const name = ctx.params.name || "world";
+  ctx.body = `Hello ${name}`;
+};
+
+router.get("/", index);
+router.get("/hello", hello);
+router.get("/hello/:name", hello);
+
+app.use(router.routes());
+
+app.listen(port).on("listening", () => console.log(`listenig on port ${port}`));
